1c43b3913145d9e21d7f2fc58cc90d9beb30c0ea
Eddy Moreno <eddymoreno10@gmail.com>
2018-05-10 01:24:59 -0600

    Added models for Likes class

--- app/src/main/java/com/apps/skadied/girlshub/models/CreateLike.java
+++ app/src/main/java/com/apps/skadied/girlshub/models/CreateLike.java
@@ -5,4 +5,24 @@ package com.apps.skadied.girlshub.models;
  */
 
 public class CreateLike {
+    private String name;
+
+    private String person_id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPerson_id() {
+        return person_id;
+    }
+
+    public void setPerson_id(String person_id) {
+        this.person_id = person_id;
+    }
+
 }
