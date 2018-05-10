6ff25fcd0f1abdab64c32727419f52a81119b8ec
Eddy Moreno <eddymoreno10@gmail.com>
2018-05-10 01:23:01 -0600

    Starting onClick fab with AddPersonActivity.class

--- app/src/main/java/com/apps/skadied/girlshub/ui/activities/Main2Activity.java
+++ app/src/main/java/com/apps/skadied/girlshub/ui/activities/Main2Activity.java
@@ -40,8 +40,8 @@ public class Main2Activity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(Main2Activity.this, AddPersonActivity.class));
+
             }
         });
 
